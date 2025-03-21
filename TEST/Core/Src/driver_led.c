fa9d546a50606c073560c1d179fa89dd30785cb5
leocro-ensea <leonard.croce@ensea.fr>
2025-03-21 14:00:59 +0100

    on repart de zero

---
+++ TEST/Core/Src/driver_led.c
@@ -0,0 +1,20 @@
+/*
+ * driver_led.c
+ *
+ *  Created on: Mar 13, 2025
+ *      Author: lcroce
+ */
+
+uint8_t MCP23S17_SPI_Transmit(uint8_t data) {
+    uint8_t receivedData;
+    HAL_SPI_TransmitReceive(&hspi3, &data, &receivedData, 1, HAL_MAX_DELAY);
+    return receivedData;
+}
+
+void MCP23S17_WriteRegister(uint8_t reg, uint8_t value) {
+    MCP23S17_CS_LOW();
+    MCP23S17_SPI_Transmit(0x40);  // Adresse + écriture (0x40 = 0b01000000)
+    MCP23S17_SPI_Transmit(reg);
+    MCP23S17_SPI_Transmit(value);
+    MCP23S17_CS_HIGH();
+}
---
+++ TEST/Core/Src/main.c
@@ -0,0 +1,254 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "spi.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+extern SPI_HandleTypeDef hspi3;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+uint8_t MCP23S17_SPI_Transmit(uint8_t data);
+void MCP23S17_WriteRegister(uint8_t reg, uint8_t value);
+uint8_t MCP23S17_ReadRegister(uint8_t reg);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+// minicom -D /dev/ttyACM-1
+// ctrl+a puis q
+
+int __io_putchar(int ch) {
+	HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+	return ch;
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_SPI3_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_GPIO_WritePin(VU_nRST_GPIO_Port, VU_nRST_Pin, SET);
+  HAL_GPIO_WritePin(VU_ncs_GPIO_Port, VU_ncs_Pin, SET);
+  MCP23S17_WriteRegister(0x00,  0x00);
+  MCP23S17_WriteRegister(0x01,  0x00);
+  int val = 255;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+
+
+  while (1)
+  {
+	     /* Faire clignoter la LED de la NUCLEO */
+	      HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	      HAL_Delay(500);
+
+	      /* Mettre GPA0 à 1 */
+	      for(int i=1; i<=8;i++)
+	      {
+	    	  val = val<<1;
+	    	  printf("val = %d", val);
+	    	  MCP23S17_WriteRegister(0x12, val);
+	    	  MCP23S17_WriteRegister(0x13, val);
+	    	  HAL_Delay(500);
+	    	  if (i==8)
+	    	 	    	  {
+	    	 	    	  	/* Mettre GPA0 à 0 */
+	 	    	  	val=0xFF;
+	    	 	    	  	MCP23S17_WriteRegister(0x12, val);
+	    	 	    	  	MCP23S17_WriteRegister(0x13, val);
+	    	 	    	  }
+	      }
+
+
+	      //printf("GPIO toggled\r\n");
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+uint8_t MCP23S17_SPI_Transmit(uint8_t data) {
+    uint8_t receivedData;
+    HAL_SPI_TransmitReceive(&hspi3, &data, &receivedData, 1, HAL_MAX_DELAY);
+    return receivedData;
+}
+
+void MCP23S17_WriteRegister(uint8_t reg, uint8_t value) {
+    MCP23S17_CS_LOW();
+    MCP23S17_SPI_Transmit(0x40);  // Adresse + écriture (0x40 = 0b01000000)
+    MCP23S17_SPI_Transmit(reg);
+    MCP23S17_SPI_Transmit(value);
+    MCP23S17_CS_HIGH();
+}
+
+/*
+	  uint8_t MCP23S17_ReadRegister(uint8_t reg) {
+    uint8_t value;
+    MCP23S17_CS_LOW();
+    MCP23S17_SPI_Transmit(0x41);  // Adresse + lecture (0x41 = 0b01000001)
+    MCP23S17_SPI_Transmit(reg);
+    value = MCP23S17_SPI_Transmit(0xFF);  // Lire la réponse
+    MCP23S17_CS_HIGH();
+    return value;
+}
+*/
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
